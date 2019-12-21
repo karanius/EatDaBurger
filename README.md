a3b184426d6a687105709ee6f937db5d1eefc5c8
Kavian Darvish <kavian1991@gmail.com>
2019-12-21 15:40:39 -0500

    configing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EatDaBurger
+http://afternoon-savannah-01200.herokuapp.com/
--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 
 const token = {
-  user: 'bc72239a23ed0c',
-  host: 'us-cdbr-iron-east-05.cleardb.net',
-  password: '54398732',
-  database: 'heroku_e824e2a7b223db0'
+  user: '',
+  host: '',
+  password: '',
+  database: ''
 }
 
 
